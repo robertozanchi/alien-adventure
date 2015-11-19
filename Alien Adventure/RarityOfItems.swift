6a970b4cfd4ec5678bb13021aee3ced321b27d9e
robertozanchi <r.zanchi@gmail.com>
2015-11-19 14:40:10 +0100

    complete RarityOfItems.swift

--- Alien Adventure/RarityOfItems.swift
+++ Alien Adventure/RarityOfItems.swift
@@ -9,6 +9,25 @@
 extension Hero {
     
     func rarityOfItems(inventory: [UDItem]) -> [UDItemRarity:Int] {
-        return [UDItemRarity:Int]()
+        
+        var itemsRarity = [UDItemRarity:Int]()
+        
+        itemsRarity[UDItemRarity.Common] = 0
+        itemsRarity[UDItemRarity.Uncommon] = 0
+        itemsRarity[UDItemRarity.Rare] = 0
+        itemsRarity[UDItemRarity.Legendary] = 0
+        
+        for item in inventory {
+            if item.rarity == UDItemRarity.Common {
+                itemsRarity[UDItemRarity.Common] = itemsRarity[UDItemRarity.Common]!+1
+            } else if item.rarity == UDItemRarity.Uncommon {
+                itemsRarity[UDItemRarity.Uncommon] = itemsRarity[UDItemRarity.Uncommon]!+1
+            } else if item.rarity == UDItemRarity.Rare {
+                itemsRarity[UDItemRarity.Rare] = itemsRarity[UDItemRarity.Rare]!+1
+            } else if item.rarity == UDItemRarity.Legendary {
+                itemsRarity[UDItemRarity.Legendary] = itemsRarity[UDItemRarity.Legendary]!+1
+            }
+        }
+        return itemsRarity
     }
 }
