f9885efd56020e7d2cc7fccbe0983047a5e43220
robertozanchi <r.zanchi@gmail.com>
2015-11-29 15:49:13 +0100

    complete PolicingItems.swift

--- Alien Adventure/PolicingItems.swift
+++ Alien Adventure/PolicingItems.swift
@@ -11,15 +11,23 @@ extension Hero {
     func policingItems(inventory: [UDItem], policingFilter: UDItem throws -> Void) -> [UDPolicingError:Int] {
         
         var errorDictionary = [UDPolicingError:Int]()
+        errorDictionary = [UDPolicingError.ItemFromCunia:0, UDPolicingError.NameContainsLaser:0, UDPolicingError.ValueLessThan10:0]
+        
+        if inventory.isEmpty {
+            return errorDictionary
+        }
+        
         for item in inventory {
             do {
                 try policingFilter(item)
             } catch UDPolicingError.ItemFromCunia {
-                
+                errorDictionary[UDPolicingError.ItemFromCunia] = (errorDictionary[UDPolicingError.ItemFromCunia])!+1
             } catch UDPolicingError.NameContainsLaser {
-                
+                errorDictionary[UDPolicingError.NameContainsLaser] = errorDictionary[UDPolicingError.NameContainsLaser]!+1
             } catch UDPolicingError.ValueLessThan10 {
-                
+                errorDictionary[UDPolicingError.ValueLessThan10] = errorDictionary[UDPolicingError.ValueLessThan10]!+1
+            } catch {
+                print("other error")
             }
         }
         return errorDictionary
